25777cea486687b96e18b58d74558eebd3a87978
Maria Nery <67118326+mnvnery@users.noreply.github.com>
2022-04-25 08:33:39 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,13 @@
 $('.project-flex').mouseover(function (e) {
     imageData = $(this).attr('data-hover');
+    linkColor = $(this).attr('data-link');
     $('.project-video').attr('src' , imageData);
     $('.project-video').addClass('full-opacity');
+    $(this).css('color', linkColor);
 });
 
 $('.project-flex').mouseout(function (e) {
     $('.project-video').attr('src' , '');
     $('.project-video').removeClass('full-opacity');
+    $('.project-flex').css('color', 'black');
 });
